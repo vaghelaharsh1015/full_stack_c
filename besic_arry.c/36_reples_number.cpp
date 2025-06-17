18be956952e68d501119bfba633b0300c369e21c
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2025-06-17 14:06:39 +0530

    new file

--- besic_arry.c/36_reples_number.cpp
+++ besic_arry.c/36_reples_number.cpp
@@ -1,18 +1,21 @@
-#include<stdio.H>
+#include<iostream>
 using namespace std;
 int  main()
 {
     int arr[]={10,-10,10,10,10};
-    int num=0;
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
+
         if (arr[i]<0)
         {
-            arr[i]==num;
-        }        
-    }
-     cout<<"zero is = "<<arr[i];
-     
+            arr[i]=0;
+        }
+
+        }   
+        for(int i=0;i<5;i++)
+        {
+        cout <<" "<<arr[i]; 
+        }
     return 0;
 }
---
+++ besic_arry.c/38_mul_in_arry.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={5,5,5};
+
+    for (int  i = 0; i <3; i++)
+    {
+        cout<<"\n"<<arr[i]*2;
+    }
+    return 0;
+}
